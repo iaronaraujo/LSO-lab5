2888933752f3b39ba9a07b4123355857b6f08b2d
Iaron Araujo <iaron.araujo@ccc.ufcg.edu.br>
2018-03-05 17:14:07 -0300

    arquivo de gerar plot pronto

---
+++ output/plot.r
@@ -0,0 +1,11 @@
+library("ggplot2")
+
+args = commandArgs(trailingOnly=TRUE)
+#data = read.csv(args[2], header = TRUE)
+data = read.csv("Output.plot", header = TRUE)
+
+summary(data)
+
+p <- ggplot(data, aes(x=buffer_size, y=moves, color=algorithm))
+p <- p + geom_line()
+ggsave(p, file=paste("resultado", "png", sep="."))
---
+++ output/trace.1.plot
@@ -0,0 +1,5 @@
+moves,buffer_size,algorithm
+50000,20,fifo
+20000,50,fifo
+30000,20,scan
+10000,50,scan
